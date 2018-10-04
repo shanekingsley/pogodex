29fb329f1c25e1e6089a2b2766d75fdf29b40887
Shane Kingsley <shane.kingsley@bcs.uk>
2018-10-04 16:01:43 +0100

    Adding dust calc

--- bot.js
+++ bot.js
@@ -1,5 +1,40 @@
 var Discord = require('discord.js');
-var auth = require('./auth.json');
+var auth = require('../.secure/auth.json');
+
+const prefix = '.';
+const calcDust = (level, special, isNew) => {
+    var baseOwned = 100;
+    var baseUnowned = 20000;
+    var baseOwnedSpecial = 20000;
+    var baseUnownedSpecial = 1000000;
+  
+    var modifiers = {
+      good : 1,
+      great: 0.8,
+      ultra: 0.08,
+      best: 0.04
+    }
+    var dustBase = 0;
+      
+    if(!special && !isNew){
+      return baseOwned; // special case
+    }
+    
+    if(!special && isNew){
+      dustBase = baseUnowned
+    }
+    
+    if(special && !isNew){
+      dustBase = baseOwnedSpecial
+    }
+    
+    if(special && isNew){
+      dustBase = baseUnownedSpecial
+    }
+    
+    return modifiers[level] * dustBase;
+  }
+
 
 // Initialize Discord Bot
 const client = new Discord.Client();
@@ -9,8 +44,26 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content.startsWith("ping")) {
-    message.channel.send("pong!");
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (message.content.startsWith(prefix)) {
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if(command === "ping"){
+        message.channel.send("pong!");
+    }
+    
+    if(command === "dust"){
+        // get the args
+        let friend = args[0];
+        let special = args[1] == "yes";
+        let isNew = args[2] == "yes";
+        
+        var dust = calcDust(friend, special, isNew)
+        message.channel.send("Dust calc... " + dust);
+    }
   }
 });
 
